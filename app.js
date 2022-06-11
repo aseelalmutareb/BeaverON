6911b6fe01447756d27da3bf445e852c9d0deb30
maykaltenev <michaeltenev@gmail.com>
2022-06-11 22:11:20 +0200

    working study div

--- app.js
+++ app.js
@@ -11,3 +11,36 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+// Case Study 
+(function () {
+    $(".flex-container").waitForImages(
+        function () {
+            $(".spinner").fadeOut();
+        },
+        $.noop,
+        true,
+    );
+
+    $(".flex-slide").each(function () {
+        $(this).hover(
+            function () {
+                $(this).find(".flex-title").css({
+                    transform: "rotate(0deg)",
+                    top: "10%",
+                });
+                $(this).find(".flex-about").css({
+                    opacity: "1",
+                });
+            },
+            function () {
+                $(this).find(".flex-title").css({
+                    transform: "rotate(90deg)",
+                    top: "15%",
+                });
+                $(this).find(".flex-about").css({
+                    opacity: "0",
+                });
+            },
+        );
+    });
+})();
